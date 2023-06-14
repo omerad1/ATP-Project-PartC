bacd9173843e20b6af47a193364faa44b2727d91
omerad1 <127669568+omerad1@users.noreply.github.com>
2023-06-14 16:15:12 +0300

    background photo

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,4 +1,26 @@
 package View;
 
+import javafx.event.ActionEvent;
+
 public class MyViewController implements IView{
+    public void NewAction(ActionEvent actionEvent) {
+    }
+
+    public void SaveAction(ActionEvent actionEvent) {
+    }
+
+    public void LoadAction(ActionEvent actionEvent) {
+    }
+
+    public void PropertiesAction(ActionEvent actionEvent) {
+    }
+
+    public void ExitAction(ActionEvent actionEvent) {
+    }
+
+    public void HelpAction(ActionEvent actionEvent) {
+    }
+
+    public void AboutAction(ActionEvent actionEvent) {
+    }
 }
--- src/View/TestView.java
+++ src/View/TestView.java
@@ -1,26 +1,38 @@
 package View;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class TestView extends Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MyView.fxml")));
+        String googleFontsCSS = "https://fonts.googleapis.com/css2?family=Diphylleia&display=swap";
+        root.getStylesheets().add(googleFontsCSS);
+
+        Label descLabel = (Label) root.lookup("#descLabel");
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
 
+
+        double percentage = 0.0215;
+        descLabel.styleProperty().bind(
+                Bindings.concat("-fx-font-size: ", stage.widthProperty().multiply(percentage), "px;")
+        );
     }
 }
+
